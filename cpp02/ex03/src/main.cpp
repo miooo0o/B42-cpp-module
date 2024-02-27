b20d4483183abcd23e48b9d7c082db8fb4e00572
minakim <minakim@student.42berlin.de>
2024-02-27 15:38:14 +0100

    feat: cpp02/ex03 main.cpp

--- cpp02/ex03/src/main.cpp
+++ cpp02/ex03/src/main.cpp
@@ -4,9 +4,48 @@
 
 #include "Point/Point.h"
 
+int	bsp_checker(Point const& a, Point const& b, Point const& c, Point const& point)
+{
+	std::cout << "Point (" << point.getX() << ", " << point.getY() << ") is ";
+	if (bsp(a, b, c, point) && bsp(b, a, c, point) && \
+			bsp(c, b, a, point) && bsp(b, c, a, point))
+	{
+		std::cout << "point inside the triangle."<< std::endl;
+		return (VALID);
+	}
+	else if (!bsp(a, b, c, point) && !bsp(b, a, c, point) && \
+			!bsp(c, b, a, point) && !bsp(b, c, a, point))
+	{
+		std::cout << "point outside the triangle."<< std::endl;
+		return (VALID);
+	}
+	std::cout << "test fail.\n"<< std::endl;
+	return (INVALID);
+}
+
 ///	@note	My First Class inOrthodox Canonical Form
 ///	Fixed-point numbers
-int main(void) {
-
+int main(void)
+{
+	Point	a(0.0f, 0.0f);
+	Point	b(2.0f, 5.0f);
+	Point	c(10.0f, 3.0f);
+	Point	point_inside(3.0f, 3.0f);
+	Point	point_outside(5.0f, 7.0f);
+	
+	std::cout << "BSP Test: Triangle ABC" << std::endl << std::endl;
+	std::cout << "Test Case 1: Point Inside Triangle" << std::endl;
+	std::cout << "Point A(" << a.getX() << ", " << a.getY() << "), "
+				<< "B(" << b.getX() << ", " << b.getY() << "), "
+				<< "C(" << c.getX() << ", " << c.getY() << ")"
+				<< std::endl;
+	bsp_checker(a, b, c, point_inside);
+	std::cout << std::endl;
+	std::cout << "Test Case 2: Point Outside Triangle" << std::endl;
+	std::cout << "Point A(" << a.getX() << ", " << a.getY() << "), "
+				<< "B(" << b.getX() << ", " << b.getY() << "), "
+				<< "C(" << c.getX() << ", " << c.getY() << ")"
+				<< std::endl;
+	bsp_checker(a, b, c, point_outside);
 	return (EXIT_SUCCESS);
 }
